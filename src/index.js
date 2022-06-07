c634daf21101f11834297dd6e95f9d9be9095188
contactus-ezygentechnology <101473970+contactus-ezygentechnology@users.noreply.github.com>
2022-06-07 12:45:47 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import "./index.css";
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
-import App from './App';
-
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
